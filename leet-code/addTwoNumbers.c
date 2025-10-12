697e8653ac490a301d46f800571959d5686a7c80
phgiacomelli <paulohenriquegiacomelli@gmail.com>
2025-10-12 19:01:48 -0300

    addTwoNumbers

--- leet-code/addTwoNumbers.c
+++ leet-code/addTwoNumbers.c
@@ -7,33 +7,20 @@ struct ListNode {
 };
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-    int sum1 = 0, sum2 = 0;
-    int fator = 1;
-    for (struct ListNode* i1 = l1; i1 != NULL; i1 = i1->next) {
-        sum1 += i1->val * fator;
-        fator *= 10;
-    }
-    fator = 1;
-    for (struct ListNode* i2 = l2; i2 != NULL; i2 = i2->next) {
-        sum2 += i2->val * fator;
-        fator *= 10;
-    }
+    int sum = 0, carry = 0;
+    struct ListNode *head = NULL, *last = NULL;;
 
-    int sum = sum1 + sum2;
-    struct ListNode *head = NULL, *last = NULL;
-    if (sum == 0)
-    {
+    while (l1 != NULL || l2 != NULL || carry > 0) {
+
+        int num1 = (l1 != NULL) ? l1->val : 0;
+        int num2 = (l2 != NULL) ? l2->val : 0;
+
+        sum = num1 + num2 + carry;
+        carry = sum / 10;
+        sum %= 10;
+
         struct ListNode* new = (struct ListNode*)malloc(sizeof(struct ListNode));
         new->val = sum;
-        new->next = NULL;
-        head = new;
-    }
-    
-    while (sum != 0) {
-        struct ListNode* new = (struct ListNode*)malloc(sizeof(struct ListNode));
-
-        new->val = sum % 10;
-        sum/=10;
         new->next = NULL;
 
         if (head == NULL) {
@@ -43,12 +30,15 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
             last->next = new;
             last = new;
         }
-    }
 
+        l1 = (l1==NULL) ? NULL: l1->next;
+        l2 = (l2==NULL) ? NULL: l2->next;
+    }
     return head;
 }
 
 struct ListNode* createList(int* arr, int size) {
+    if (size == 0) return NULL;
 
     struct ListNode* head = malloc(sizeof(struct ListNode));
     head->val = arr[0];
@@ -56,24 +46,39 @@ struct ListNode* createList(int* arr, int size) {
 
     struct ListNode* current = head;
     for (int i = 1; i < size; i++) {
-        struct ListNode* new = malloc(sizeof(struct ListNode));
-        new->val = arr[i];
-        new->next = NULL;
-        current->next = new;
-        current = new;
+        struct ListNode* newNode = malloc(sizeof(struct ListNode));
+        newNode->val = arr[i];
+        newNode->next = NULL;
+        current->next = newNode;
+        current = newNode;
     }
 
     return head;
 }
 
+void printList(struct ListNode* head) {
+    while (head != NULL) {
+        printf("%d", head->val);
+        if (head->next) 
+            printf(" -> ");
+        head = head->next;
+    }
+    printf("\n");
+}
+
 int main() {
-    int arr1[] = {0};
-    int arr2[] = {0};
+    int arr1[] = {9};
+    // int arr2[] = {1, 9};
+    int arr2[] = {1, 9, 9, 9, 9, 9, 9, 9, 9, 9};
 
     struct ListNode* l1 = createList(arr1, 1);
-    struct ListNode* l2 = createList(arr2, 1);
+    // struct ListNode* l2 = createList(arr2, 2);
+    struct ListNode* l2 = createList(arr2, 10);
 
     struct ListNode* result = addTwoNumbers(l1, l2);
 
+    printf("Resultado: ");
+    printList(result);
+    free(result);
     return 0;
 }
